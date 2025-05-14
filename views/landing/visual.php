12e4e31f54ae135060d35631ab511e58bd1513a8
413xsali <alexsanchezli.2005@gmail.com>
2025-05-14 16:19:37 -0500

    actualice mi rama

---
+++ views/landing/visual.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title> HOME | Libros WAP</title>
+    <link rel="stylesheet" href="/FINAL-LIBROS-WAP/public/styles.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="logo-container">
+            <img src="/FINAL-LIBROS-WAP/public/img/logow.png" alt="Logo" class="logo">
+            <span class="titulo">LIBROS WAP</span>
+        </div>
+        <div class="nav-links">
+            <a href="/FINAL-LIBROS-WAP/views/landing/visual.php">Home</a>
+            <a href="/FINAL-LIBROS-WAP/views/landing/servicios.php">Servicios</a>
+            <a href="/FINAL-LIBROS-WAP/views/login/login.php">Login</a>
+            <a href="/FINAL-LIBROS-WAP/views/login/registro.php">Registrarse</a>
+            <a href="/FINAL-LIBROS-WAP/views/landing/contact.php">Contactanos</a>
+        </div>
+        
+    </header>
+
+    <main class="main-section">
+        <div class="banner-libros">
+            <img src="/FINAL-LIBROS-WAP/public/img/3.png" alt="Libro 1">
+        </div>
+
+        <section class="descripcion">
+            <h2>Todo esta a tu alcance</h2>
+            <p>En este aplicativo puedes navegar, tener información sobre la alta gama de contenido, para comprar, vender o intercambiar gustos</p>
+        </section>
+
+        <div class="libros-destacados">
+            <img src="/FINAL-LIBROS-WAP/public/img/2.jpg" alt="Destacado 1">
+            <img src="/FINAL-LIBROS-WAP/public/img/1.jpg" alt="Destacado 2">
+            <img src="/FINAL-LIBROS-WAP/public/img/4.jpg" alt="Destacado 3">
+        </div>
+      
+
+    </main>
+</body>
+</html>
--- views/login/registro.php
+++ views/login/registro.php
@@ -1,4 +1,13 @@
-<div class="row justify-content-center">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/FINAL-LIBROS-WAP/public/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card">
             <div class="card-header bg-primary text-white">
@@ -14,23 +23,23 @@
                 <form action="index.php?controller=auth&action=registro" method="POST">
                     <div class="mb-3">
                         <label for="nombre" class="form-label">Nombre</label>
-                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Asignar nombre" required>
                     </div>
                     <div class="mb-3">
                         <label for="apellido" class="form-label">Apellido</label>
-                        <input type="text" class="form-control" id="apellido" name="apellido" required>
+                        <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Asignar Apellido" required>
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Asignar correo electronico" required>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Asignar contraseña" required>
                     </div>
                     <div class="mb-3">
                         <label for="confirmar_password" class="form-label">Confirmar Contraseña</label>
-                        <input type="password" class="form-control" id="confirmar_password" name="confirmar_password" required>
+                        <input type="password" class="form-control" id="confirmar_password" name="confirmar_password" placeholder="Confirmar contraseña" required>
                     </div>
                     <div class="d-grid gap-2">
                         <button type="submit" class="btn btn-primary">Registrarse</button>
@@ -38,9 +47,12 @@
                 </form>
                 
                 <div class="mt-3 text-center">
-                    <p>¿Ya tienes una cuenta? <a href="/FINALWAP/views/login/login.php">Iniciar Sesión</a></p>
+                    <p>¿Ya tienes una cuenta? <a href="/FINAL-LIBROS-WAP/views/login/login.php">Iniciar Sesión</a></p>
+                    <a href="/FINAL-LIBROS-WAP/views/landing/visual.php">Volver</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</body>
+</html>
