9f6977f7f13bdd4c3ecf686f3564b1d4fe1a90b5
Ana Carreño <aelixabeth201@gmail.com>
2025-05-21 13:55:48 -0500

    Agrgue nuevos metodos al modelo del carrito de compras

--- models/carro.php
+++ models/carro.php
@@ -7,7 +7,7 @@ class Carro{
         $this->db=$db;
         $this->usuario_id=$usuario_id;
     }
-
+    // Obtener solo un libro
     public function obteneridcarro(){
         $stmt = $this->db->prepare("SELECT id FROM carrito WHERE usuario_id = ?");
         $stmt->execute([$this->usuario_id]);
@@ -22,10 +22,29 @@ class Carro{
         }
     }
 
+    // Listar todos los libros
+    public function obtenertodos($libro_id){
+
+
+    }
+
+    // Agregar un libro al carrito
     public function agregarlibro($libro_id,$cantidad){
         $carrito_id = $this->obteneridcarro();
 
         $stmt = $this->db->prepare("SELECT id, cantidad FROM ci.id, l.titulo,lprecio,ci.cantidad FROM carrito_items ci JOIN libros l ON ci.libro_id = l.id WHERE ci.carrito_id =?");
+    }
+
+
+    // Eliminar un libro del carrito
+    public function eliminarlibro($libro_id){
+
+    }
+    
+    // Vaciar todo el carrito
+    public function vaciarcarrito($carrito_id){
+
+
     }
 
 }
